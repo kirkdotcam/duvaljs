8ee50f09eea6e9895eb5a6db478ac6a8cda77902
CamKirk <camden.kirkland@gmail.com>
2020-01-18 10:56:54 -0600

    WIP: clicking in a region works

--- public/pentagon.js
+++ public/pentagon.js
@@ -8,19 +8,20 @@ let height = svg.attr('height');
 let centerX = width / 2;
 let centerY = height / 2;
 let numSides = 5;
-let radius = height / 3;
+let radius = height / 4;
 let globalPoints = {};
+let maxGasScale = 40; //as defined by paper, no gas concentration will likely rise above 40%
 
-let zonepoints = d3.json('./pentagonbasic.json')
-  .then((res)=>{
+let zonepoints = d3.json('./pentagonreverse.json')
+  .then((res) => {
     globalPoints = res
-    drawZones(res)
+    // drawZones(res)
   });
 
 
 drawFrame();
 
-function drawFrame(){
+function drawFrame() {
 
   let segments = d3.range(numSides)
     .map(i => {
@@ -35,27 +36,26 @@ function drawFrame(){
     });
 
   segments.push(segments[0]);
-  
+
   let lineFunction = d3.line()
-    .x(d=>d[0])
-    .y(d=>d[1]);
+    .x(d => d[0])
+    .y(d => d[1]);
 
   svg.append("path")
     .attr("d", lineFunction(segments))
-    .attr("stroke","blue")
+    .attr("stroke", "blue")
     .attr("stroke-width", 3)
     .attr("fill", "none");
 }
 
-function drawZones(zoneSet){
-  console.log(zoneSet);
-  console.log("rad", radius);
-
+function drawZones(zoneSet) {
+  
   let lineFunction = d3.line()
-    .x(d=> calcAngle(d, "x")+ centerX ) // need to scale the x and y values up to the radius of the frame SOLUTION: convert to polar, scale, convert back to cartesian
-    .y(d=> (-d.y)+centerY);
+    .x(d => d.x * (radius/maxGasScale) + centerX)
+    .y(d => -d.y *(radius/maxGasScale)+ centerY);
 
   for (const key in zoneSet) {
+    zoneSet[key].push(zoneSet[key][0])
     svg.append("path")
     .attr("d", lineFunction(zoneSet[key]))
     .attr("stroke","black")
@@ -63,64 +63,66 @@ function drawZones(zoneSet){
     .attr("fill", "none");
   }
 
+  console.log(zoneSet)
+
 }
 
-function calcAngle(d, mode){
-  let x = d.x;
-  let y = d.y;
-
-  let r = Math.sqrt(Math.pow(x, 2), Math.pow(y, 2));
-  let angle = Math.atan(d.x, d.y);
-
-  let scalar = radius/r;
-  console.log(scalar, angle, r);
-  
-  if (mode === "x"){
-    return scalar * Math.cos(angle);
-  }
-
-  if (mode === "y"){
-    return scalar * Math.sin(angle)
-  }
-
-
-  // console.log(angle)
-  // if (mode === "x"){
-  //   return Math.sin(angle)*radius;
-  // }
-  // if (mode === "y"){
-  //   return Math.cos(angle)*radius;
-  // }
-}
-
-function drawPoint(){
+function drawPoint() {
 
 }
 
 let newpoints = []
 
 //transform zonepoints
-zonepoints.then(()=>{
-  newpoints = Object.values(globalPoints).map((shape)=>{
-    return shape.map((point)=>{
+zonepoints.then(() => {
+  
+  newpoints = Object.values(globalPoints).map((shape) => {
+    return shape.map((point) => {
       return Object.values(point)
     })
   })
 
-  newpoints.forEach((shape)=>{
+  newpoints.forEach((shape) => {
     shape.push(shape[0]);
   })
 
+  newpoints = newpoints.map((shape)=>{
+
+    return shape.map(point=>{
+      return [
+        point[0] * (radius/maxGasScale) + centerX,
+        -point[1] * (radius/maxGasScale) + centerY
+      ]
+    })
+
+  })
   console.log(newpoints)
+
+
+
+let lineFunction2 = d3.line()
+.x(d => d[0])
+.y(d => d[1]);
+
+  newpoints.forEach(shape =>{
+    svg.append("path")
+    .attr("d", lineFunction2(shape))
+    .attr("stroke","green")
+    .attr("stroke-width", 3)
+    .attr("fill", "none");
+  })
 })
 
-d3.select('svg').on('click',()=>{
-  let {x,y} = d3.event
-  console.log("pixels",x,y);
-  
-  console.log([x-centerX,y-centerY])//need to SCALE UP THE NEWPOINTS FOR THE CLICK LOCATION
-  newpoints.forEach((zone)=>{
-    console.log(d3.polygonContains(zone,[x-centerX,y-centerY])) //polygoncontains broken due to change of coordinate system
+//need to convert pixels of click back to area of pentagon, or areas of pentagon to click
+d3.select('svg').on('click', () => {
+  let { x, y } = d3.event
+  console.log("pixels", x, y);
+  console.log(x*(radius/maxGasScale)-centerX)
+  let i = 0;
+  console.log("relative to pentagon", [x - centerX, y - centerY])//need to SCALE UP THE NEWPOINTS FOR THE CLICK LOCATION
+  newpoints.forEach((zone) => {
+    console.log(i,d3.polygonContains(zone, [x,y])) //polygoncontains broken due to change of coordinate system
+    ++i
   })
 })
 
@@ -128,9 +130,9 @@ svg.append('line')
   .attr('x1', _ => 20)
   .attr('y1', _ => 20)
   .attr('x2', _ => 250)
-  .attr('y2', _=> 250)
-  .attr('stroke','red')
-  .attr('stroke-width',3);
+  .attr('y2', _ => 250)
+  .attr('stroke', 'red')
+  .attr('stroke-width', 3);
 
 // https://bl.ocks.org/curran/8b4b7791fc25cfd2c459e74f3d0423f2
 
