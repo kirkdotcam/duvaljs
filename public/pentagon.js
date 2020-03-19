a1a3eeeec24d884a4da9203856f84d29388b69d0
CamKirk <camden.kirkland@gmail.com>
2020-03-18 22:43:01 -0500

    add form to html, starting form submit, but need to finish calcCentroid

--- public/pentagon.js
+++ public/pentagon.js
@@ -11,7 +11,7 @@ let numSides = 5;
 let radius = height / 4;
 let globalPoints = {};
 let maxGasScale = 40; //as defined by paper, no gas concentration will likely rise above 40%
-
+let frameAngles = [];
 let zonepoints = d3.json('./pentagonreverse.json')
   .then((res) => {
     drawZones(res)
@@ -26,6 +26,7 @@ function drawFrame() {
     .map(i => {
 
       let angle = i / numSides * (Math.PI * 2) + Math.PI;
+      frameAngles.push(angle); 
 
       return [
         Math.sin(angle) * radius + centerX,
@@ -69,8 +70,8 @@ function drawZones(zoneSet) {
 
   }
 
-  console.log(zoneSet)
-  console.log(globalPoints)
+  // console.log(zoneSet)
+  // console.log(globalPoints)
 
 }
 
@@ -81,27 +82,59 @@ function drawPoint(x,y) {
     .attr("r",1.5)
 }
 
-function calcCentroid() {
+function calcCentroid(gasPercentArray) {
+  // calc surface area
+  gasPercentArray.map((curr,idx)=>{
+    return {
+      r:curr,
+      angle:frameAngles[idx]
+    }
+  }).reduce((acc,curr,idx,src)=>{
+    let x1 = curr.r * Math.cos(curr.angle);
+    let y1 = curr.r * Math.cos((Math.PI/2)-curr.angle);
+
+
+    let nextRef = idx === src.length ? 0 : idx+1;
+
+    let x2 = src[nextRef] * Math.cos(curr.angle);
+    let y2 = src[nextRef] * Math.cos((Math.PI/2)-curr.angle);
+
+    return acc + (x1*y2 - x2*y1)
+
+  })
+  
+
+  // calc x and y
 
 }
 
 function determineZone(x,y) {
   Object.entries(globalPoints).forEach((zone) => {
-    console.log(zone[0], d3.polygonContains(zone[1], [x, y]));
+    if (d3.polygonContains(zone[1], [x, y])){
+      console.log(zone[0])
+    }
   })
 }
+function formSubmit(){
+  let form = d3.selectAll("input")
+  form.each((d,i,nodes)=>{
+    console.log(nodes[i].value)
+  })
 
+}
+
+// d3.select('#submission').on('click', () =>{
+
+// })
 //need to convert pixels of click back to area of pentagon, or areas of pentagon to click
 d3.select('svg').on('mousedown', () => {
+  console.clear();
 
   let { x, y } = d3.event
+  determineZone(x,y);
   x = x-10
   y = y-10
-  console.clear();
-  console.log(x,y);
-  console.log(Object.entries(globalPoints));
-  drawPoint(x,y)
-  determineZone(x,y);
+  drawPoint(x,y);
 })
 
 // https://bl.ocks.org/curran/8b4b7791fc25cfd2c459e74f3d0423f2
